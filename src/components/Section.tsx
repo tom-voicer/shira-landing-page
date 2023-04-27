3186cb304a59e0b1ad3a36b59c2762c99d180b77
Tom Sitton <tom@voicer.com>
2023-04-27 21:30:47 +0300

    add image viewer

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 
-import 'react-image-lightbox/style.css';
-
-import Lightbox from 'react-image-lightbox';
-
 import getRootUrl from '../utils/rootUrl';
 
 export interface ISectionProps {
@@ -85,21 +81,47 @@ export default function Section(props: ISectionProps) {
       <p>{props.description}</p>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {props.images.map((image, index) => (
+        {props.images.map((img, index) => (
           <img
             onClick={() => setPhotoIndex(index)}
-            key={image}
+            key={img}
             className="cursor-pointer h-auto max-w-full rounded-lg"
-            src={`${rootUrl}/assets/images/sections/${props.anchor}/${image}`}
+            src={`${rootUrl}/assets/images/sections/${props.anchor}/${img}`}
             alt=""
           ></img>
         ))}
-        {photoIndex !== -1 && (
-          <Lightbox
-            mainSrc={`${rootUrl}/assets/images/sections/${props.anchor}/${props.images[photoIndex]}`}
-            onCloseRequest={() => setPhotoIndex(-1)}
-          />
-        )}
+      </div>
+
+      <div
+        style={{ zIndex: 1000 }}
+        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 ${
+          photoIndex !== -1 ? '' : 'hidden'
+        }`}
+      >
+        <div
+          className="flex justify-center items-center h-full"
+          onClick={() => setPhotoIndex(-1)}
+        >
+          <div className="max-w-full max-h-full">
+            <button
+              className="absolute top-0 right-0 m-4"
+              onClick={() => setPhotoIndex(-1)}
+            >
+              <svg
+                className="w-6 h-6 fill-current text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 10.586L6.293 4.879a1 1 0 10-1.414 1.414L10.586 12l-5.707 5.707a1 1 0 101.414 1.414L12 13.414l5.707 5.707a1 1 0 101.414-1.414L13.414 12l5.707-5.707a1 1 0 00-1.414-1.414L12 10.586z" />
+              </svg>
+            </button>
+            <img
+              className="max-w-screen max-h-screen mx-auto"
+              src={`${rootUrl}/assets/images/sections/${props.anchor}/${props.images[photoIndex]}`}
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
