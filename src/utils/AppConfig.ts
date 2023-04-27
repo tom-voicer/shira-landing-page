2633c18a5b68c0bf265c6cf457d9c57ab88f560f
Tom Sitton <tom@voicer.com>
2023-04-27 21:42:32 +0300

    ;;

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,9 +1,8 @@
 export const AppConfig = {
   site_name: 'קוראת בין השורות',
   title: 'קוראת בין השורות',
-  description:
-    'Starter code for your Next.js blog Boilerplate with Tailwind CSS',
-  url: 'https://example.com',
+  description: 'שירותי כתיבת תוכן, המלצות, הרצאות ומיקוד מסרים',
+  url: 'https://koret-ben-hashurot.netlify.app/',
   locale: 'en',
   author: 'Anonymous',
   pagination_size: 5,
